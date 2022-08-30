df34129479bd5abc83ae4e3f90a399f7693e978f
Adam Mattheis <oldcartoons@Adams-MBP.attlocal.net>
2022-08-29 20:26:58 -0400

    first commit

---
+++ game.js
@@ -0,0 +1,40 @@
+// function - type out text letter by letter. used for title page, page 2, and post-game page 
+
+
+
+// function - clicking "PLAY" button triggers page 2 to replace title page
+
+
+
+// function - clicking "START" button triggers game canvas to replace page 2
+
+
+
+// function - post-game page is triggered when lives = 0 or enemies = 21 (all enemies destroyed)
+
+
+
+// finction - score counter = total destroyed enemy points + time bonus
+
+
+
+// function - player movement left or right with arrow keys
+
+
+
+// function - shoots bullet with space bar
+
+
+
+// function - enemyGreen movement
+
+
+
+// function - enemyBlue movement
+
+
+// function - enemyRed movement 
+
+
+
+// function - enemyGold movement
