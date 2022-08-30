7e98ae583d4019c834c93d77e74fc017fa512184
Adam Mattheis <oldcartoons@Adams-MBP.attlocal.net>
2022-08-30 01:43:25 -0400

    title page animation

--- game.js
+++ game.js
@@ -1,6 +1,91 @@
 // function - type out text letter by letter. used for title page, page 2, and post-game page 
 
 
+function typeoutColor1(){
+    document.getElementById("t1").style.color='whitesmoke'
+}
+
+function typeoutColor2(){
+    document.getElementById("t2").style.color='whitesmoke'
+}
+
+function typeoutColor3(){
+    document.getElementById("t3").style.color='whitesmoke'
+}
+
+function typeoutColor4(){
+    document.getElementById("t4").style.color='whitesmoke'
+}
+
+function typeoutColor5(){
+    document.getElementById("t5").style.color='whitesmoke'      
+}
+
+function typeoutColor6(){
+    document.getElementById("t6").style.color='whitesmoke'
+}
+
+function typeoutColor7(){
+    document.getElementById("t7").style.color='whitesmoke'
+}
+
+function typeoutColor8(){
+    document.getElementById("t8").style.color='whitesmoke'
+}
+
+function typeoutColor9(){
+    document.getElementById("t9").style.color='whitesmoke'
+}
+
+function typeoutColor10(){
+    document.getElementById("t10").style.color='whitesmoke'
+}
+
+function typeoutColor11(){
+    document.getElementById("t11").style.color='whitesmoke'
+}
+
+function typeoutColor12(){
+    document.getElementById("t12").style.color='whitesmoke'
+}
+
+function typeoutColor13(){
+    document.getElementById("t13").style.color='whitesmoke'
+}
+
+function typeoutColor14(){
+    document.getElementById("t14").style.color='whitesmoke'
+}
+
+function typeoutColor15(){
+    document.getElementById("t15").style.color='whitesmoke'
+}
+
+function typeoutColor16(){
+    document.getElementById("t16").style.color='whitesmoke'
+}
+
+function typeoutColor17(){
+    document.getElementById("t17").style.color='whitesmoke'
+}
+
+setTimeout(typeoutColor1,150)
+setTimeout(typeoutColor2,300)
+setTimeout(typeoutColor3,450)
+setTimeout(typeoutColor4,600)
+setTimeout(typeoutColor5,750)
+setTimeout(typeoutColor6,900)
+setTimeout(typeoutColor7,1050)
+setTimeout(typeoutColor8,1200)
+setTimeout(typeoutColor9,1350)
+setTimeout(typeoutColor10,1500)
+setTimeout(typeoutColor11,1650)
+setTimeout(typeoutColor12,1800)
+setTimeout(typeoutColor13,1950)
+setTimeout(typeoutColor14,2100)
+setTimeout(typeoutColor15,2250)
+setTimeout(typeoutColor16,2400)
+setTimeout(typeoutColor17,2550)
 
 // function - clicking "PLAY" button triggers page 2 to replace title page
 
@@ -54,8 +139,16 @@
 
 
 
-// function - +1 life trigger = enemies >= 10 and/or destroy gold enemy
+// function - +1 life trigger = enemies >= 10 and/or gold enemy = 1
 
 
 
-// function - 
+// function - display "victory!" or "game over" enemies = 21 or lives = 0
+
+
+
+// function - display "final score" (score + time bonus)
+
+
+
+// 
